be0aa1e1d4d39a1ec642af3e988ddf8610a4721f
jesse459 <lptvvdm@gmail.com>
2020-10-26 12:48:27 +0300

    Translated user interface in English, added a feature of counting cathetus with known hypothenuse and angle, added more comments

--- triangles.py
+++ triangles.py
@@ -8,14 +8,34 @@ def cath(c, a): # counts cathetus
    b = math.sqrt(c**2 - a**2)
    return b
 
+def cath_cos(c, x): #counts cathetus with a known angle measured in degrees
+    rad = math.radians(x) #converts the output in degrees to radians
+    cos = math.cos(x) #counts the cos of an angle
+    cat = c * cos #counts the cathetus
+    return a
+
+
+
 while True:
-   action = str(input("Что ты хочешь сделать? \n 1 - посчитать гипотенузу \n 2 - найти катет \n"))
+   action = str(input(
+       "Choose an action, print an appropriate number and press enter\n"
+       "1 - count hypothenuse\n"
+       "2 - count cathetus\n"
+       "3 - count cathetus with known cos"
+
+
+
+
+
+   ))
    if action == "1":
-      a = float(input("Введите значение первого катета"))
-      b = float(input("Введите значение второго катета"))
+      a = float(input("Enter first cathetus"))
+      b = float(input("Enter second cathetus"))
       print(hypo(a, b))
    elif action == "2":
-      c = float(input("Введите значение гипотенузы"))
-      a = float(input("Введите значение катета"))
+      c = float(input("Enter hypothenuse"))
+      a = float(input("Enter cathetus"))
       print(cath(c, a))
+   elif action == "3":
+       c = float(input("Enter hypothenuse"))
 
