7595f266a1c4755b0777b0c6a532674406b10cfa
jesse459 <lptvvdm@gmail.com>
2020-10-26 13:37:37 +0300

    added cath_sin function, made user interface more accurate. Made a result to be converted into string to be able to concatenate it with "The result is: "

--- triangles.py
+++ triangles.py
@@ -12,7 +12,13 @@ def cath_cos(c, x): #counts cathetus with a known angle measured in degrees
     rad = math.radians(x) #converts the output in degrees to radians
     cos = math.cos(x) #counts the cos of an angle
     cat = c * cos #counts the cathetus
-    return a
+    return cat
+
+def cath_sin(c, x): #counts cathetus with a known angle measured in degrees
+    rad = math.radians(x)  # converts the output in degrees to radians
+    sin = math.sin(x)  # counts the sin of an angle
+    cat = c * sin  # counts the cathetus
+    return cat
 
 
 
@@ -21,7 +27,9 @@ while True:
        "Choose an action, print an appropriate number and press enter\n"
        "1 - count hypothenuse\n"
        "2 - count cathetus\n"
-       "3 - count cathetus with known cos"
+       "3 - count cathetus with cos\n"
+       "4 - count cathetus with sin\n "
+       " "
 
 
 
@@ -29,13 +37,23 @@ while True:
 
    ))
    if action == "1":
-      a = float(input("Enter first cathetus"))
-      b = float(input("Enter second cathetus"))
-      print(hypo(a, b))
+      a = float(input("Enter first cathetus: "))
+      b = float(input("Enter second cathetus: "))
+      res = hypo(a, b)
+      print("The result is: " + str(res))
    elif action == "2":
-      c = float(input("Enter hypothenuse"))
-      a = float(input("Enter cathetus"))
-      print(cath(c, a))
+      c = float(input("Enter hypothenuse: "))
+      a = float(input("Enter cathetus: "))
+      res = cath(c, a)
+      print("The result is: " + str(res))
    elif action == "3":
-       c = float(input("Enter hypothenuse"))
+       c = float(input("Enter hypothenuse: "))
+       x = float(input("Enter angle in degrees: "))
+       res = cath_cos(c, x)
+       print("The result is: " + str(res))
+   elif action == "4":
+       c = float(input("Enter hypothenuse: "))
+       x = float(input("Enter angle in degrees: "))
+       res = cath_sin(c, x)
+       print("The result is: " + str(res))
 
