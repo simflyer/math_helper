42352945ad4369b631b50383f478441a9cc4a834
jesse459 <lptvvdm@gmail.com>
2020-10-27 11:25:55 +0300

    Added try-except for ValueError exception, added sleep function

--- triangles.py
+++ triangles.py
@@ -1,3 +1,4 @@
+import time
 import math
 
 def hypo(a, b): # counts hypothenuse
@@ -24,6 +25,7 @@ def cath_sin(c, x): #counts cathetus with a known angle measured in degrees
 
 while True:
    action = str(input(
+       " \n "
        "Choose an action, print an appropriate number and press enter\n"
        "1 - count hypothenuse\n"
        "2 - count cathetus\n"
@@ -41,19 +43,30 @@ while True:
       b = float(input("Enter second cathetus: "))
       res = hypo(a, b)
       print("The result is: " + str(res))
+      time.sleep(2)
    elif action == "2":
-      c = float(input("Enter hypothenuse: "))
-      a = float(input("Enter cathetus: "))
-      res = cath(c, a)
-      print("The result is: " + str(res))
+       try:
+            c = float(input("Enter hypothenuse: "))
+            a = float(input("Enter cathetus: "))
+            res = cath(c, a)
+            print("The result is: " + str(res))
+            time.sleep(2)
+       except ValueError:
+           print("It seems that hypothenuse is less than cathetus. Try again!")
+           time.sleep(2)
    elif action == "3":
        c = float(input("Enter hypothenuse: "))
        x = float(input("Enter angle in degrees: "))
        res = cath_cos(c, x)
        print("The result is: " + str(res))
+       time.sleep(2)
    elif action == "4":
        c = float(input("Enter hypothenuse: "))
        x = float(input("Enter angle in degrees: "))
        res = cath_sin(c, x)
        print("The result is: " + str(res))
+       time.sleep(2)
+   else:
+       print("There is no such an action. Try again")
+       time.sleep(2)
 
