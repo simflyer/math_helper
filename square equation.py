25a1a07e8279519e584c32d320958a963af79cf6
jesse459 <lptvvdm@gmail.com>
2020-10-27 12:07:04 +0300

    Added sleep time to square_equation.py

--- square equation.py
+++ square equation.py
@@ -1,3 +1,4 @@
+import time
 import math
 def root(a, b, c): #gets an input of square equation terms and counts the root
    d = b**2 - 4 * a * c
@@ -11,10 +12,13 @@ while True:
         a = float(input("Enter first term (a): "))
         if a == 0:
             print("It is not square equation, first term can`t be 0")
+            time.sleep(2)
         else:
             b = float(input("Enter second term (b): "))
             c = float(input("Enter third term (c): "))
             res = root(a, b, c)
             print("Here is the result: " + str(res))
+            time.sleep(2)
     except ValueError:
         print("This equation does not have root")
+        time.sleep(2)
